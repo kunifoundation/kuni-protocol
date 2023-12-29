3545d823755629bf6b37782d7a29105e38c6b793
thuandv68 <94217873+thuandv68@users.noreply.github.com>
2023-12-29 20:47:13 +0700

    Add eco-game deploy script

---
+++ scripts/01.1.deploy.eco.js
@@ -0,0 +1,34 @@
+const {ethers} = require("hardhat");
+
+const TOKENS = require("./contract.json")
+const loadContract = require('./attach-contract')
+
+
+const {deployContract} = ethers;
+
+const log = console.log;
+async function main() {
+    const [deployer, ...addrs] = await ethers.getSigners();
+    const BALANCE_START = await ethers.provider.getBalance(deployer.address);
+    let nonce = await ethers.provider.getTransactionCount(deployer.address);
+    const core = await loadContract()
+    log(`======= DEPLOY..... ========\n`);
+    log("DEPLOY COMMON...");
+    this.eco = await (await deployContract("EcoGame", [TOKENS.metaData])).waitForDeployment({nonce: ++nonce});
+    const ecoGame = await this.eco.getAddress();
+    log("4. EcoGame: ", ecoGame);
+    
+    await (await core.game.setEco(ecoGame, {nonce: ++nonce})).wait()
+    log('set eco for game')
+    await (await core.inv.setEco(ecoGame, {nonce: ++nonce})).wait()
+    log('set eco for inv')
+    log("\n=========== TOKEN DEPLOYED! ===========\n");
+
+    log("Balance Fee: ", ethers.formatEther(BALANCE_START - (await ethers.provider.getBalance(deployer.address))), "BNB");
+    TOKENS.ecoGame = ecoGame
+    writeWithToken(TOKENS, "_contract-eco.js", 0, "json");
+}
+
+main()
+    .catch((err) => log(err))
+    .then(() => log("========= DEPLOYED! ========="));
